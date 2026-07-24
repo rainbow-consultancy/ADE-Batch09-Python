c4b5fd9f1de1d8bdaf9ce45ac8ff9ce23d02264e
rainbow-consultancy <tech.cloudcraft@gmail.com>
2026-07-24 09:38:45 +0530

    added 2 new topics

---
+++ conditions.py
@@ -0,0 +1,25 @@
+# conditions 
+
+num = int(input("Enter the num: "))
+
+# if num % 2 == 0:
+#     print(f"{num} is a Even number")
+# else:
+#     print(f"{num} is a Odd number")
+
+# if num < 0:
+#     print(f"{num} is a negative number, please enter a positive number")
+# elif num % 2 == 0:
+#     print(f"{num} is a Even number")
+# else:
+#     print(f"{num} is a Odd number")
+
+# print(f"{num} is a Even number" if num % 2 == 0 else f"{num} is a Odd number")
+
+
+
+
+    
+
+
+
