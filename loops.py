61f90b784dca5f5b2bb348b24bcc58ef249cc6c2
rainbow-consultancy <tech.cloudcraft@gmail.com>
2026-07-24 09:40:57 +0530

    .

--- loops.py
+++ loops.py
@@ -10,18 +10,18 @@
 # for i in str, list, dict, tuple:
 
 
-# for i in range(1, 51, 2):
-#     print(i)
+for i in range(1, 51, 2):
+    print(i)
 
-# name = "Python"
-# nums = [1, 2, 3, 4, 5]
+name = "Python"
+nums = [1, 2, 3, 4, 5]
 
-# for ch in nums:
-#     print(ch)
+for ch in nums:
+    print(ch)
     
 
-# for i in range(51):
-#     print(i)
+for i in range(51):
+    print(i)
 
 
 # while loop
@@ -30,21 +30,21 @@
 
 # while condition:
 
-# i = 1
-# while i < 51:
-#     print(i)
-#     i = i+1
+i = 1
+while i < 51:
+    print(i)
+    i = i+1
     
 
 # list comprehension
 
 # --> print even numbers between 1 to 30 and append in a list
 
-# even = []
-# for i in range(1, 31):
-#     if i % 2 == 0:
-#         even.append(i)
-# print(even)
+even = []
+for i in range(1, 31):
+    if i % 2 == 0:
+        even.append(i)
+print(even)
 
 even = [i for i in range(1, 31) if i % 2 == 0]
 print(even)
